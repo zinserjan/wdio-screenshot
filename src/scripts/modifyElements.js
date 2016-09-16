5f3fbc0bea6eca56de191f354be4937a14466369
Jan-André Zinser <zinserjan@gmail.com>
2016-09-16 20:07:04 +0200

    !important suffix for styles (#27)
    * Added !important suffix for styles
    
    * missing tests for hide & remove

--- src/scripts/modifyElements.js
+++ src/scripts/modifyElements.js
@@ -9,7 +9,14 @@ export default function modifyElements() {
   args.splice(-2);
   for (var i = 0; i < args.length; ++i) {
     for (var j = 0; j < args[i].length; ++j) {
-      args[i][j].style[style] = value;
+      var element = args[i][j];
+
+      try {
+         element.style.setProperty(style, value, 'important');
+      }
+      catch (error) {
+         element.setAttribute('style', element.style.cssText + style + ':' + value + '!important;');
+      }
     }
   }
 }
