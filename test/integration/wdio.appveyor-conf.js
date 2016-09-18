a3c8a2ee85abb2e680766ac7c92ab9b992dc8ee0
Jan-André Zinser <zinserjan@gmail.com>
2016-09-18 20:07:44 +0200

    use firefox, chrome & internet explorer for Appveyor CI (#30)

---
+++ test/integration/wdio.appveyor-conf.js
@@ -0,0 +1,44 @@
+require("babel-register");
+var path = require('path');
+
+exports.config = {
+  specs: [
+    path.join(__dirname, '/specs/desktop.test.js')
+  ],
+  capabilities: [
+    {
+      browserName: 'chrome'
+    },
+    {
+      browserName: 'firefox'
+    },
+    {
+      browserName: 'internet explorer',
+      version: '11'
+    }
+  ],
+  sync: false,
+  logLevel: 'silent',
+  coloredLogs: true,
+  baseUrl: 'http://zinserjan.github.io/wdio-screenshot/integration',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000,
+    compilers: [
+      'js:babel-register'
+    ],
+  },
+  before: function() {
+    require('../../src').init(browser, {})
+  },
+  services: ['selenium-standalone'],
+  seleniumArgs: {
+    seleniumArgs: [
+      '-Djna.nosys=true'
+    ]
+  }
+}
