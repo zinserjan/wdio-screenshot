3126ce115b6088a1927f5fdcac56c95531b7b8eb
Alexander Abashkin <monolithed@gmail.com>
2016-09-20 15:55:13 +0300

    Added JSDoc annotations #33 (#36)

--- src/commands/saveDocumentScreenshot.js
+++ src/commands/saveDocumentScreenshot.js
@@ -1,11 +1,17 @@
 import makeDocumentScreenshot from '../modules/makeDocumentScreenshot';
 import saveBase64Image from '../utils/saveBase64Image';
 
+/**
+ * @alias browser.saveDocumentScreenshot
+ * @param {string} fileName
+ * @param {Object} options
+ */
+
 // Note: function name must be async to signalize WebdriverIO that this function returns a promise
-export default async function async(browser, fileName, options = {}) {
+export default async function async(fileName, options = {}) {
 
   // make screenshot of area
-  const base64Image = await makeDocumentScreenshot(browser, options);
+  const base64Image = await makeDocumentScreenshot(this, options);
 
   // store base64 image as real png
   await saveBase64Image(fileName, base64Image);
