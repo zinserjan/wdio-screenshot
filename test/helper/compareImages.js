ffc8ad9b3bea6ca949ef02cafe080caf298f1723
Jan-André Zinser <zinserjan@gmail.com>
2016-10-09 22:39:51 +0200

    allow custom misMatchPercentage for compare helper

--- test/helper/compareImages.js
+++ test/helper/compareImages.js
@@ -3,12 +3,12 @@ import {
 } from 'chai';
 import resemble from 'node-resemble-js';
 
-export default function compareImages(image1, image2) {
+export default function compareImages(image1, image2, misMatchPercentage = 0.2) {
   return new Promise((resolve) => {
     const image = resemble(image1).compareTo(image2);
     image.onComplete((data) => {
       assert.isTrue(data.isSameDimensions, `different dimensions, see "${image1}" and "${image2}"`);
-      assert.closeTo(Number(data.misMatchPercentage), 0, 0.2, `different images, see "${image1}" and "${image2}"`);
+      assert.closeTo(Number(data.misMatchPercentage), 0, misMatchPercentage, `different images, see "${image1}" and "${image2}"`);
       resolve();
     });
   });
