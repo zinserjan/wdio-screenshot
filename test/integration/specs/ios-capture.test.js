5cc1d0b4e856ed68a5d4ed534d652bf74dc439dc
Jan-André Zinser <zinserjan@gmail.com>
2016-10-11 21:15:57 +0200

    improve ios capturing of fixture screenshots

--- test/integration/specs/ios-capture.test.js
+++ test/integration/specs/ios-capture.test.js
@@ -9,31 +9,32 @@ import compareImages from '../../helper/compareImages';
 
 const screenshotDir = path.join(process.cwd(), '.tmp');
 
-function getSpecificFile(type, file) {
+function getSpecificFile(page, type, file) {
   const { deviceName, deviceOrientation, platformName, platformVersion } = browser.desiredCapabilities;
 
-  const folder = path.join(platformName, `_v${platformVersion}`, deviceName, type, file).replace(/\s/, '_');
+  const folder = path.join(platformName, `v${platformVersion}`, deviceName, `${page}_${type}`.replace(/-/, '_'), file).replace(/\s/, '_');
   return path.join(screenshotDir, folder);
 }
 
-async function takeIt(type) {
+async function takeIt(page, type) {
   const base64Screenshot = (await browser.screenshot()).value;
-  await saveBase64Image(getSpecificFile(type, 'screenshot.png'), base64Screenshot);
+  await saveBase64Image(getSpecificFile(page, type, 'screenshot.png'), base64Screenshot);
   const screenDimensions = (await browser.execute(getScreenDimensions)).value;
-  await fsExtra.outputFile(getSpecificFile(type, 'dimensions.json'), JSON.stringify(screenDimensions, null, 2));
+  await fsExtra.outputFile(getSpecificFile(page, type, 'dimensions.json'), JSON.stringify(screenDimensions, null, 2));
 }
 
 
 describe('capture ios screenshots & dimensions for unit tests', function () {
 
   beforeEach(async function () {
-    await browser.url('/empty.html');
+    this.page = 'empty';
+    await browser.url(`/${this.page}.html`);
     await browser.pause(5000);
   });
 
   it('default', async function () {
     const orientation = (await browser.orientation()).value.toLowerCase();
-    await takeIt(`started_in_${orientation}`);
+    await takeIt(this.page, `started_in_${orientation}`);
   });
 
   it('rotates', async function () {
@@ -42,7 +43,7 @@ describe('capture ios screenshots & dimensions for unit tests', function () {
 
     await browser.orientation(opposite);
     await browser.pause(1000);
-    await takeIt(`started_in_${orientation}_rotated_to_${opposite}`);
+    await takeIt(this.page, `started_in_${orientation}_rotated_to_${opposite}`);
   });
 
   it('rotates back', async function () {
@@ -50,7 +51,7 @@ describe('capture ios screenshots & dimensions for unit tests', function () {
     const opposite = orientation === 'landscape' ? 'portrait' : 'landscape';
 
     await browser.orientation(opposite);
-    await takeIt(`started_in_${opposite}_rotated_back`);
+    await takeIt(this.page, `started_in_${opposite}_rotated_back`);
 
   });
 
